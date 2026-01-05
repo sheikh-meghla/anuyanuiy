2b4edd68bdacdc7629b41e6fda47875212134b86
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-05 19:47:58 +0600

    modify

---
+++ modify_strings.py
@@ -0,0 +1,19 @@
+#Upper
+a = "Meghla, Sheikh"
+print(a.upper())
+
+#Lower
+a = "Meghla, Sheikh"
+print(a.lower())
+
+#strip
+a = "Meghla, Sheikh"
+print(a.strip())
+
+#replace
+a = "Meghla, Sheikh"
+print(a.replace("M","L"))
+
+#split
+a = "Meghla, Sheikh"
+print(a.split(","))
