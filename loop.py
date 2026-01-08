970bc4407ddc2bda8e29c3d138eed1434993495c
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-08 20:26:39 +0600

    loop

---
+++ loop.py
@@ -0,0 +1,9 @@
+thislist = ["Meghla", "Liza", "Nurani", "Rafi", "Tasnim", "Sheikh", "Ruhi"]
+for i in range(len(thislist)):
+    print(thislist[i])
+    
+# Alternative using while loop
+    i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
