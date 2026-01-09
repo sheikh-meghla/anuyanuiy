ab1efc85638bd76691d1a8aaf2ee0492858b2f3c
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-09 20:26:25 +0600

    loop tuples

---
+++ loop_tuples.py
@@ -0,0 +1,15 @@
+thistuples = ("apple", "banana", "cherry")
+i = 0
+
+while i < len(thistuples):
+    print(i, thistuples[i])
+    i += 1
+
+print("   ")
+
+i = 0
+for fruit in thistuples:
+    print(i, fruit)
+    i += 1
+
+
