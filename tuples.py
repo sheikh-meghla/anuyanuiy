8efb08334ab646d3283581483d0e017fbbbaba60
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-09 19:34:20 +0600

    tuples

---
+++ tuples.py
@@ -0,0 +1,14 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+
+thistuple = ("apple")
+print(type(thistuple))
